35f0880493889e6eab6c8beb213ec49c00fd8db8
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-28 01:27:13 +0700

    first

---
+++ src/main/java/com/belajar/java/csv/LearnCsvApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.java.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCsvApplication.class, args);
+	}
+
+}
